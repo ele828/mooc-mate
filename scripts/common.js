05c0e5d5086f30c85b73c21ae04c025f92155543
ele828 <dongjiemusic@qq.com>
2015-11-08 11:55:34 +0800

    fix

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,9 @@
-var root = "http://10.209.202.84:8080/"
+var root = "http://10.209.202.152:8080"
 var api = {
-    login: root + "api/login/"
+    login: root + "/api/login",
+    create: root + "/api/video/add",
+    fetch: root + "/api/video/get",
+    star: root + "/api/video/star",
 }
 var log = function(msg) {
     console.log(msg);
@@ -50,8 +53,22 @@ var Utils = (function() {
                     }
                 });
             });
-        }
+        },
 
+        screenShot: function(__player) {
+            var canvas = document.createElement("canvas");
+            var scale = 0.5;
+            canvas.width = __player.videoWidth * scale;
+            canvas.height = __player.videoHeight * scale;
+            canvas.getContext('2d')
+               .drawImage(__player, 0, 0, canvas.width, canvas.height);
+            var img = document.createElement("img");
+            img.crossOrigin = "Anonymous";
+            img.crossOrigin = "*";
+            img.src = canvas.toDataURL("image/png");
+            $("body").append(img)
+            // console.log( img );
+        }
 
     }
 })();
